803940dd08bc02cd867c79c3ed090b07642bfe41
roman <roman.yakhimovich@gmail.com>
2026-01-01 14:33:45 +0200

    Set up initial Gradle project with Kotlin, including Hangman game implementation and basic IDE configuration files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,42 @@
+fun main() {
+    while (true) {
+        val words = mutableListOf("apple", "banana", "orange")
+        val guessedWord = words.random()
+        val hidden = CharArray(guessedWord.length) { '_' }
+        var lives = 5
+        while (lives > 0) {
+            println("Word: ${hidden.joinToString(" ")}")
+            println("Lives left: $lives")
+            print("Enter your first letter: ")
+            val input = readln()
+            if (input.isEmpty()) {
+                continue
+            }
+            val letter = input[0]
+            var found = false
+            for (i in guessedWord.indices) {
+                if (letter == guessedWord[i]) {
+                    hidden[i] = letter
+                    found = true
+                }
+            }
+            if (!found) {
+                lives--
+                println("No such letter in the word!")
+            }
+            if ('_' !in hidden) {
+                println("You won! The word was $guessedWord")
+                break
+            }
+        }
+        if (lives == 0) {
+            println("You lost! The word was $guessedWord")
+            break
+        }
+        print("Play again? (y/n): ")
+        if (readln() != "y") {
+            println("Thanks for playing!")
+            break
+        }
+    }
+}
